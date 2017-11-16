4bfa6a26256dfb3f3fd573f4f31d13b5e45bc84d
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-11-16 13:10:49 -0500

    Initial commit

---
+++ src/main/java/edu/oakland/csi3450/Csi3450Application.java
@@ -0,0 +1,12 @@
+package edu.oakland.csi3450;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csi3450Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csi3450Application.class, args);
+	}
+}
---
+++ src/test/java/edu/oakland/csi3450/Csi3450ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.oakland.csi3450;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Csi3450ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
