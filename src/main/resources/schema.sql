87a4c97325b672ad43fcc1a4ee4c8b36d3a88451
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-11-28 11:13:42 -0500

    Creation of remaining tables #9

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,3 +19,40 @@ CREATE TABLE IF NOT EXISTS aircraft (
     routing_range numeric(7) NOT NULL,
     name varchar(25)
 );
+
+CREATE TABLE IF NOT EXISTS contact (
+    contact_id serial PRIMARY KEY,
+    ct_first_name varchar(25) NOT NULL,
+    ct_last_name varchar(25) NOT NULL,
+    ct_phone_number numeric(10) NOT NULL,
+    ct_email varchar(25) NOT NULL,
+    ct_relationship varchar(15)
+);
+      
+CREATE TABLE IF NOT EXISTS job (
+    job_id serial PRIMARY KEY,
+    job_type varchar(25) NOT NULL,
+    salary numeric(6) NOT NULL
+);
+    
+CREATE TABLE IF NOT EXISTS reservation (
+    reservation_id serial PRIMARY KEY,
+    luggage_weight numeric(2) NOT NULL,
+    seat_number varchar(4) NOT NULL,
+    accommodations varchar(25),
+    aircraft_id serial references aircraft,
+    invoice_id serial references payment,
+    insurance boolean NOT NULL
+);
+    
+CREATE TABLE IF NOT EXISTS payment (
+    invoice_id serial PRIMARY KEY,
+    vendor_name varchar(25) NOT NULL,
+    vendor_id numeric(7) NOT NULL,
+    method varchar(7) NOT NULL
+);
+    
+CREATE TABLE IF NOT EXISTS cancellations (
+    reservation_id serial references reservation,
+    cancel_date date NOT NULL
+);
