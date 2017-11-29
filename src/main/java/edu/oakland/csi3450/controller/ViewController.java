b2e68f389ecb69f0a9d6ab1ee5a545ee1efe05ef
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-11-29 13:20:56 -0500

    Add ViewController

---
+++ src/main/java/edu/oakland/csi3450/controller/ViewController.java
@@ -0,0 +1,14 @@
+package edu.oakland.csi3450.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/customer")
+    public String customer(Model model) {
+        return "customer";
+    }
+}
